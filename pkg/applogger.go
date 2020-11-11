9e3ae53c730b7efb9d30ce98728556019cc2be37
will.allen@venafi.com <will.allen@venafi.com>
2020-11-11 22:35:34 +0000

    Create applogger.go

---
+++ pkg/applogger.go
@@ -0,0 +1,131 @@
+package simplelogger
+
+import (
+	sli "github.com/tdrip/logger/pkg/interfaces"
+)
+
+type AppLogger struct {
+
+	//inherit from interface
+	sli.IAppLogger
+
+	Log     sli.ISimpleLogger `json:"-"`
+	Started bool              `json:"-"`
+}
+
+/*
+	START/FINISH LOG FUNCTIONS
+*/
+
+// the logging functions are here
+func (al *AppLogger) StartLogging() {
+
+	if al.Log == nil {
+		log := NewApplicationNowLogger()
+
+		// lets open a file log using the session
+		log.OpenAllChannels()
+
+		al.Log = log
+	} else {
+		al.Log.OpenAllChannels()
+	}
+
+	al.Started = true
+}
+
+// the logging functions are here
+func (al *AppLogger) FinishLogging() {
+
+	if al.Log != nil {
+		al.Log.CloseAllChannels()
+	}
+}
+
+/*
+	APP LOG FUNCTIONS
+*/
+
+// the logging functions are here
+func (al *AppLogger) LogDebug(cmd string, data ...interface{}) {
+	if !al.Started {
+		al.StartLogging()
+	}
+	if al.Log != nil {
+		al.Log.LogDebug(cmd, data)
+	}
+}
+
+func (al *AppLogger) LogWarn(cmd string, data ...interface{}) {
+	if !al.Started {
+		al.StartLogging()
+	}
+	if al.Log != nil {
+		al.Log.LogWarn(cmd, data)
+	}
+}
+
+func (al *AppLogger) LogInfo(cmd string, data ...interface{}) {
+	if !al.Started {
+		al.StartLogging()
+	}
+	if al.Log != nil {
+		al.Log.LogInfo(cmd, data)
+	}
+}
+
+func (al *AppLogger) LogError(cmd string, data ...interface{}) {
+	if !al.Started {
+		al.StartLogging()
+	}
+	if al.Log != nil {
+		al.Log.LogError(cmd, data)
+	}
+}
+
+// This Log error allows errors to be logged .Error() is the data written
+func (al *AppLogger) LogErrorE(cmd string, data error) {
+	if !al.Started {
+		al.StartLogging()
+	}
+	if al.Log != nil {
+		al.Log.LogErrorE(cmd, data)
+	}
+}
+
+// the logging functions are here
+func (al *AppLogger) LogDebugf(cmd string, msg string, data ...interface{}) {
+	if !al.Started {
+		al.StartLogging()
+	}
+	if al.Log != nil {
+		al.Log.LogDebugf(cmd, msg, data)
+	}
+}
+
+func (al *AppLogger) LogWarnf(cmd string, msg string, data ...interface{}) {
+	if !al.Started {
+		al.StartLogging()
+	}
+	if al.Log != nil {
+		al.Log.LogWarnf(cmd, msg, data)
+	}
+}
+
+func (al *AppLogger) LogInfof(cmd string, msg string, data ...interface{}) {
+	if !al.Started {
+		al.StartLogging()
+	}
+	if al.Log != nil {
+		al.Log.LogInfof(cmd, msg, data)
+	}
+}
+
+func (al *AppLogger) LogErrorf(cmd string, msg string, data ...interface{}) {
+	if !al.Started {
+		al.StartLogging()
+	}
+	if al.Log != nil {
+		al.Log.LogErrorf(cmd, msg, data)
+	}
+}
