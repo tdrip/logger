09be42be0a76ac2bbc8eb72999e894a636af56b5
will.allen@venafi.com <will.allen@venafi.com>
2020-11-11 22:35:18 +0000

    Create iapplogger.go

---
+++ pkg/interfaces/iapplogger.go
@@ -0,0 +1,18 @@
+package slinterfaces
+
+// main interface for the SimpleLogger
+type IAppLogger interface {
+	LogErrorf(cmd string, message string, data ...interface{})
+	LogWarnf(cmd string, message string, data ...interface{})
+	LogInfof(cmd string, message string, data ...interface{})
+	LogDebugf(cmd string, message string, data ...interface{})
+
+	LogError(cmd string, data ...interface{})
+	LogErrorE(cmd string, data error)
+	LogWarn(cmd string, data ...interface{})
+	LogInfo(cmd string, data ...interface{})
+	LogDebug(cmd string, data ...interface{})
+
+	StartLogging()
+	FinishLogging()
+}
